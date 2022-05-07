f20bdc8f49191638e76d3f36d3ae8f95f49c9c59
thai.vo <43738511+ht0201@users.noreply.github.com>
2022-05-07 18:27:09 +0700

    SSG and Data Fetching on client side

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,65 @@
-import * as React from 'react';
+// import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+
+// const Header = dynamic(() => import('./components/common/header'), {
+//   ssr: false,
+// });
 
 export interface AboutPageProps {}
 
-export default function About(props: AboutPageProps) {
-  return <div>AboutPageProps</div>;
+export default function AboutPage(props: AboutPageProps) {
+  const router = useRouter();
+  const [posts, setPosts] = useState([]);
+
+  console.log('About query', router.query);
+  const page = router.query?.page;
+
+  useEffect(() => {
+    if (!page) {
+      router.push(`/about?page=1`);
+      return;
+    }
+
+    (async () => {
+      const res = await fetch(
+        `https://js-post-api.herokuapp.com/api/posts?_page=${page}`
+      );
+      const data = await res.json();
+      setPosts(data.data);
+    })();
+  }, [page]);
+
+  const handleNextPage = () => {
+    router.push(
+      {
+        pathname: './about',
+        query: {
+          page: (Number(page) || 1) + 1,
+        },
+      },
+      undefined,
+      { shallow: true } // ko render server
+    );
+  };
+  return (
+    <div>
+      <h1>About</h1>
+      {/* <Header /> */}
+
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+      <button onClick={handleNextPage}>Next page</button>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  console.log('GET STATIC PROPS');
+  return {
+    props: {},
+  };
 }
