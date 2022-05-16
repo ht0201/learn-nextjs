57f25602bb3304acd294ee294be55f82453b8d4a
thai.vo <43738511+ht0201@users.noreply.github.com>
2022-05-16 11:35:04 +0700

    Footer

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,12 +1,36 @@
-import { Box } from '@mui/material';
-import * as React from 'react';
+import { Facebook, Instagram, LinkedIn, Twitter } from '@mui/icons-material'
+import { Box, Icon, Stack, Typography } from '@mui/material'
+import * as React from 'react'
 
 export interface FooterProps {}
 
 export function Footer(props: FooterProps) {
-  return (
-    <Box component='footer' py={2} textAlign='center'>
-      Footer
-    </Box>
-  );
+	const socialLinks = [
+		{ icon: Facebook, url: 'https://google.com' },
+		{ icon: Instagram, url: 'https://google.com' },
+		{ icon: Twitter, url: 'https://google.com' },
+		{ icon: LinkedIn, url: 'https://google.com' },
+	]
+	return (
+		<Box component="footer" py={2} textAlign="center">
+			<Stack direction="row" justifyContent="center">
+				{socialLinks.map((social, index) => (
+					<Box
+						key={index}
+						component="a"
+						p={2}
+						href={social.url}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<Icon component={social.icon} sx={{ fontSize: 48 }} />
+					</Box>
+				))}
+			</Stack>
+
+			<Typography>
+				Copyright ©{new Date().getFullYear()} All rights reserved{' '}
+			</Typography>
+		</Box>
+	)
 }
